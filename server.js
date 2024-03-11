04ef1db3e51807055cb16b352079235013876f25
7chii <marinpacper@gmail.com>
2024-03-10 23:14:52 -0300

    .

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8080;
+
+app.use('/static', express.static('static'));
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'static/index.html'));
+})
+app.get('/static/styles.css',(req, res)=>
+{
+    res.set('content-type', 'text/css');
+    res.sendFile(path.join(__dirname, 'static/styles.css'))
+})
+app.get('/aula02',(req, res)=>{
+    res.sendFile(path.join(__dirname,'static/aula2.html'))
+})
+app.get('/aval',(req, res)=>{
+    res.sendFile(path.join(__dirname,'static/aval.html'))
+})
+app.get('/ativ',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'static/ativ.html'))
+})
+app.get('/aula03',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'static/aula3.html'))
+})
+app.listen(port, ()=>{
+    console.log(`api listening on port ${port}`)
+})
